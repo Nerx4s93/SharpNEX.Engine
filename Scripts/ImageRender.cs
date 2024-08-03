8c932ac1da76681ea59c04b66a8897bcc4d81b6e
Никита Хуснутдинов <nikitahysnutdinov@gmail.com>
2024-08-03 19:17:45 +0300

    Скрипт для рендеринга изображения объекта

---
+++ Scripts/ImageRender.cs
@@ -0,0 +1,21 @@
+﻿namespace SharpNEX.Engine.Scripts
+{
+    public class ImageRender : Script
+    {
+        public string Image;
+
+        public Vector DeltaPosition;
+        public Quartion DeltaRotation;
+
+        public override void Update()
+        {
+            if (string.IsNullOrEmpty(Image))
+            {
+                return;
+            }
+
+            Game.Render(Image, GameObject.Position, GameObject.Rotation);
+            GameObject.Rotation = new Quartion(GameObject.Rotation.Angle + 0.05f);
+        }
+    }
+}
