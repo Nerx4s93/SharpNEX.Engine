9217b67a44cc31240c7c5f07cb3aa8ee626d4b82
Никита Хуснутдинов <nikitahysnutdinov@gmail.com>
2024-08-03 19:21:41 +0300

    Сылки на Position и Quartion внутри скрипта

--- Script.cs
+++ Script.cs
@@ -4,6 +4,9 @@
     {
         public GameObject GameObject { get; internal set; }
 
+        public Vector Position => GameObject.Position;
+        public Quartion Rotation => GameObject.Rotation;
+
         public bool IsScriptStarted { get; internal set; }
 
         public virtual void Start() { }
