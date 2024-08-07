3135882aa911b9e55f8f6226e8ccfe0fcca9a1f9
Никита Хуснутдинов <nikitahysnutdinov@gmail.com>
2024-08-07 06:59:28 +0300

    Добавлены операторы инверсии, умножения, деления

--- Vector.cs
+++ Vector.cs
@@ -22,5 +22,20 @@
         {
             return new Vector(a.X - b.X, a.Y - b.Y);
         }
+
+        public static Vector operator -(Vector a)
+        {
+            return new Vector(-a.X, -a.Y);
+        }
+
+        public static Vector operator *(Vector a, int b)
+        {
+            return new Vector(a.X * b, a.Y * b);
+        }
+
+        public static Vector operator /(Vector a, int b)
+        {
+            return new Vector(a.X / b, a.Y / b);
+        }
     }
 }
