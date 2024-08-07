ecbbd5e5d8ad2b9a55025960488a6af9a7d83eeb
Никита Хуснутдинов <nikitahysnutdinov@gmail.com>
2024-08-07 06:32:38 +0300

    Класс отвечающий за хитбокс объекта

---
+++ Scripts/BoxHitbox.cs
@@ -0,0 +1,18 @@
+﻿namespace SharpNEX.Engine.Scripts
+{
+    public class BoxHitbox : HitboxBase
+    {
+        public override void Start()
+        {
+            Points.Add(new Vector(0, 0));
+            Points.Add(new Vector(HitboxSize, 0));
+            Points.Add(new Vector(HitboxSize, HitboxSize));
+            Points.Add(new Vector(0, HitboxSize));
+        }
+
+        public override void Update()
+        {
+            Draw();
+        }
+    }
+}
---
+++ Scripts/HitboxBase.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+using SharpDX.Mathematics.Interop;
+
+namespace SharpNEX.Engine.Scripts
+{
+    public class HitboxBase : Script
+    {
+        public float HitboxSize;
+        public Vector DeltaPosition;
+
+        protected List<Vector> Points = new List<Vector>();
+
+        protected void Draw()
+        {
+            for (int i = 0; i < Points.Count; i++)
+            {
+                Vector pointStart = Position + Points[i];
+                Vector pointEnd = Position + (i == Points.Count - 1 ? Points[0] : Points[i + 1]);
+
+                Game.GpaphicsRender.DrawLine(pointStart, pointEnd, 1f, new RawColor4(0, 1, 0, 1));
+            }
+        }
+    }
+}
