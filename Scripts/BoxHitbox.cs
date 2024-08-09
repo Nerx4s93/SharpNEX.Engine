662ad5ebcbabea924145d779955a66caff33cbaf
Никита Хуснутдинов <nikitahysnutdinov@gmail.com>
2024-08-09 15:43:42 +0300

    Возможность установить размер хитбокса по X и Y

--- Scripts/BoxHitbox.cs
+++ Scripts/BoxHitbox.cs
@@ -2,19 +2,51 @@
 {
     public class BoxHitbox : HitboxBase
     {
-        public override void Start()
-        {
-            float halfHitboxSize = HitboxSize / 2;
+        private float _hitboxSizeX;
+        private float _hitboxSizeY;
 
-            Points.Add(new Vector(-halfHitboxSize, -halfHitboxSize));
-            Points.Add(new Vector(halfHitboxSize, -halfHitboxSize));
-            Points.Add(new Vector(halfHitboxSize, halfHitboxSize));
-            Points.Add(new Vector(-halfHitboxSize, halfHitboxSize));
+        public float HitboxSizeX
+        {
+            get
+            {
+                return _hitboxSizeX;
+            }
+            set
+            {
+                _hitboxSizeX = value;
+                UpdateHitbox();
+            }
+        }
+
+        public float HitboxSizeY
+        {
+            get
+            {
+                return _hitboxSizeY;
+            }
+            set
+            {
+                _hitboxSizeY = value;
+                UpdateHitbox();
+            }
         }
 
         public override void Update()
         {
             Draw();
+        }
+
+        private void UpdateHitbox()
+        {
+            Points.Clear();
+
+            float halfHitboxSizeX = _hitboxSizeX / 2;
+            float halfHitboxSizeY = _hitboxSizeY / 2;
+
+            Points.Add(new Vector(-halfHitboxSizeX, -halfHitboxSizeY));
+            Points.Add(new Vector(halfHitboxSizeX, -halfHitboxSizeY));
+            Points.Add(new Vector(halfHitboxSizeX, halfHitboxSizeY));
+            Points.Add(new Vector(-halfHitboxSizeX, halfHitboxSizeY));
         }
     }
 }
