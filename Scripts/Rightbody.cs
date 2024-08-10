8c0bb1d7d113250d7970517a8e07e06c7e44fd76
Никита Хуснутдинов <nikitahysnutdinov@gmail.com>
2024-08-10 06:29:15 +0300

    Скрипт отвещающий за физику объекта

---
+++ Scripts/Rightbody.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+using SharpNEX.Engine.Utils;
+
+#pragma warning disable CS0168
+
+namespace SharpNEX.Engine.Scripts
+{
+    public class Rightbody : Script
+    {
+        private HitboxBase _hitboxBase;
+
+        public override void Start()
+        {
+            _hitboxBase = GameObject.GetScriptFromBaseType<HitboxBase>();
+        }
+
+        public override void Update()
+        {
+            List<GameObject> gameObjects = Game.Scene.GameObjects;
+
+            foreach (GameObject gameObject in gameObjects)
+            {
+                if (gameObject == GameObject)
+                {
+                    continue;
+                }
+
+                try
+                {
+                    HitboxBase hitboxBase = gameObject.GetScriptFromBaseType<HitboxBase>();
+
+                    bool colision = CollisionCalculator.ColisionHitboxes(_hitboxBase, hitboxBase);
+                    //TODO: отталкивание
+                }
+                catch (InvalidOperationException invalidOperationException) { }
+            }
+        }
+    }
+}
