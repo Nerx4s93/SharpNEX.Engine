dbfeee767e642d67a8780afaa0768f531eb5c1eb
Никита Хуснутдинов <nikitahysnutdinov@gmail.com>
2024-08-11 11:04:28 +0300

    Возможность получить длину вектора и нормализовать его

--- Vector.cs
+++ Vector.cs
@@ -1,4 +1,6 @@
-﻿namespace SharpNEX.Engine
+﻿using System;
+
+namespace SharpNEX.Engine
 {
     public struct Vector
     {
@@ -12,6 +14,8 @@
         public float Y;
 
         public static Vector Zero = new Vector(0, 0);
+
+        #region Операторы
 
         public static Vector operator +(Vector a, Vector b)
         {
@@ -38,9 +42,24 @@
             return new Vector(a.X / b, a.Y / b);
         }
 
+        #endregion
+
         public override string ToString()
         {
             return $"{{ X: {X}; Y: {Y} }}";
+        }
+
+        public float GetLenght()
+        {
+            return (float)Math.Sqrt(X * X + Y * Y);
+        }
+
+        public Vector Normalize()
+        {
+            float length = GetLenght();
+
+            Vector result = new Vector(X / length, Y / length);
+            return result;
         }
     }
 }
