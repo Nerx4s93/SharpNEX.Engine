21096583d2f043d32d9a0ef6ef4a823afd774c2f
Nerx <nikitahysnutdinov@gmail.com>
2024-11-24 19:55:23 +0300

    Класс для логов игры

---
+++ Debug.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SharpNEX.Engine
+{
+    // TODO:
+    //
+    // public static void LogError()
+    // public static void Save()
+    // Log handle
+    public static class Debug
+    {
+        private static readonly List<Message> messages = new List<Message>();
+        private static string stringFormat;
+
+        public static void Log(string text)
+        {
+            var message = new Message(DateTime.Now, text);
+            messages.Add(message);
+            stringFormat += "\n" + message.ToString();
+        }
+
+        public static void Clear()
+        {
+            messages.Clear();
+            stringFormat = string.Empty;
+        }
+
+        public static string GetAllText()
+        {
+            return stringFormat;
+        }
+
+        public static string GetLine(int index)
+        {
+            return messages[index].ToString();
+        }
+
+        public class Message
+        {
+            public Message(DateTime dateTime, string text)
+            {
+                DateTime = dateTime;
+                Text = text;
+            }
+
+            public readonly DateTime DateTime;
+            public readonly string Text;
+
+            public override string ToString()
+            {
+                return $"[{DateTime.ToString("T")}] {Text}";
+            }
+        }
+    }
+}
