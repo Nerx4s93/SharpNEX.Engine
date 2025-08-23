68c6b5fdbc9c2fd7acc0e6da86a9403ad9a8dc37
Никита Хуснутдинов <nikitahysnutdinov@gmail.com>
2025-08-23 09:08:43 +0300

    Класс дл подсчёта тиков в секунду

---
+++ UPS.cs
@@ -0,0 +1,23 @@
+﻿namespace SharpNEX.Engine;
+
+internal class UPS
+{
+    private int _sum = 0;
+    private readonly Queue<int> _ticks = new Queue<int>();
+
+    public void AddTik(int tik)
+    {
+        _ticks.Enqueue(tik);
+        _sum += tik;
+
+        while (_sum >= 1000)
+        {
+            _sum -= _ticks.Dequeue();
+        }
+    }
+
+    public int GetUPS()
+    {
+        return _ticks.Count;
+    }
+}
