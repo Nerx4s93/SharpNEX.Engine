11ff932711380f9c2f164208c26f73411a797a46
Никита Хуснутдинов <nikitahysnutdinov@gmail.com>
2025-08-23 09:35:06 +0300

    Класс игровогообъекта

---
+++ GameObject.cs
@@ -0,0 +1,70 @@
+﻿namespace SharpNEX.Engine;
+
+[Serializable]
+internal class GameObject(string name, List<Script> scripts)
+{
+    private readonly List<Script> _scripts = scripts;
+
+    private Vector _position;
+    private Rotation _rotation;
+    private Vector _size;
+
+    public string Name = name;
+
+    public GameObject(string name) : this(name, []) { }
+
+    public Vector Position
+    {
+        get => _position;
+        set => _position = value;
+    }
+
+    public Rotation Rotation
+    {
+        get => _rotation;
+        set => _rotation = value;
+    }
+
+    public Vector Size
+    {
+        get => _size;
+        set => _size = value;
+    }
+
+    #region Scripts
+
+    public bool HasScript<T>()
+    {
+        return _scripts.Select(script => script.GetType()).
+            Any(scriptType => typeof(T) == scriptType);
+    }
+
+    public bool HasScriptFromBaseType<T>()
+    {
+        return _scripts.Any(s => s is T);
+    }
+
+    public T GetScript<T>() where T : Script
+    {
+        var script = _scripts.FirstOrDefault(s => s.GetType() == typeof(T));
+        if (script == null)
+        {
+            throw new InvalidOperationException($"Script \"{typeof(T).Name}\" is not found.");
+        }
+
+        return (T)script;
+    }
+
+    public T GetScriptFromBaseType<T>() where T : Script
+    {
+        var script = _scripts.FirstOrDefault(s => s is T);
+        if (script == null)
+        {
+            throw new InvalidOperationException($"Script \"{typeof(T).Name}\" is not found.");
+        }
+
+        return (T)script;
+    }
+
+    #endregion
+}
