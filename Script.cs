e783d2857ff64b23a78b4f56b343a6b1d4ddc196
Никита Хуснутдинов <nikitahysnutdinov@gmail.com>
2025-08-23 09:42:41 +0300

    Класс скрипта

--- Script.cs
+++ Script.cs
@@ -1,12 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace SharpNEX.Engine;
 
-namespace SharpNEX.Engine
+internal class Script
 {
-    internal class Script
+    public GameObject GameObject { get; internal set; }
+
+    #region Описание игрового объекта
+
+    public string Name
     {
+        get => GameObject.Name;
+        set => GameObject.Name = value;
     }
+
+    public Vector Position
+    {
+        get => GameObject.Position;
+        set => GameObject.Position = value;
+    }
+
+    public Rotation Rotation
+    {
+        get => GameObject.Rotation;
+        set => GameObject.Rotation = value;
+    }
+
+    public Vector Size
+    {
+        get => GameObject.Size;
+        set => GameObject.Size = value;
+    }
+
+    #endregion
+
+    public virtual void Start() { }
+    public virtual void Update() { }
 }
