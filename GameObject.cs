cfefb92c32917cf72e359578308cb10b94f5793d
Никита Хуснутдинов <nikitahysnutdinov@gmail.com>
2025-08-23 10:06:44 +0300

    Зависимости объектов

--- GameObject.cs
+++ GameObject.cs
@@ -1,15 +1,20 @@
-﻿namespace SharpNEX.Engine;
+﻿using System.Collections.ObjectModel;
+
+namespace SharpNEX.Engine;
 
 [Serializable]
 internal class GameObject(string name, List<Script> scripts)
 {
     private readonly List<Script> _scripts = scripts;
+    private readonly HashSet<GameObject> _children = [];
 
     private Vector _position;
     private Rotation _rotation;
     private Vector _size;
 
     public string Name = name;
+
+    public GameObject? Parent { get; private set; }
 
     public GameObject(string name) : this(name, []) { }
 
@@ -37,37 +42,59 @@ internal class GameObject(string name, List<Script> scripts)
 
     #region Скрипты
 
-    public bool HasScript<T>()
-    {
-        return _scripts.Select(script => script.GetType()).
-            Any(scriptType => typeof(T) == scriptType);
-    }
+    public ReadOnlyCollection<Script> GetScripts() => _scripts.AsReadOnly();
 
-    public bool HasScriptFromBaseType<T>()
-    {
-        return _scripts.Any(s => s is T);
-    }
+    public bool HasScript<T>() => _scripts.Any(s => s.GetType() == typeof(T));
+
+    public bool HasScriptFromBaseType<T>() => _scripts.Any(s => s is T);
 
     public T GetScript<T>() where T : Script
     {
-        var script = _scripts.FirstOrDefault(s => s.GetType() == typeof(T));
-        if (script == null)
-        {
-            throw new InvalidOperationException($"Script \"{typeof(T).Name}\" is not found.");
-        }
-
+        var script = _scripts.FirstOrDefault(s => s.GetType() == typeof(T))
+                     ?? throw new InvalidOperationException($"Script \"{typeof(T).Name}\" is not found.");
         return (T)script;
     }
 
     public T GetScriptFromBaseType<T>() where T : Script
     {
-        var script = _scripts.FirstOrDefault(s => s is T);
-        if (script == null)
+        var script = _scripts.FirstOrDefault(s => s is T)
+                     ?? throw new InvalidOperationException($"Script \"{typeof(T).Name}\" is not found.");
+        return (T)script;
+    }
+
+    #endregion
+
+    #region Зависимости
+
+    public IReadOnlyCollection<GameObject> Children => _children;
+
+    public void AddChild(GameObject? child)
+    {
+        if (child == null || child == this)
         {
-            throw new InvalidOperationException($"Script \"{typeof(T).Name}\" is not found.");
+            return;
         }
 
-        return (T)script;
+        child.Parent?.RemoveChild(child);
+        child.Parent = this;
+        _children.Add(child);
+    }
+
+    public void RemoveChild(GameObject? child)
+    {
+        if (child == null || child.Parent != this)
+        {
+            return;
+        }
+
+        child.Parent = null;
+        _children.Remove(child);
+    }
+
+    public void SetParent(GameObject? newParent)
+    {
+        Parent?.RemoveChild(this);
+        newParent?.AddChild(this);
     }
 
     #endregion
