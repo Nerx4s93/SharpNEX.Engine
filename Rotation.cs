b29c3a2442ba6c3a00a97d57506d06e812645878
Никита Хуснутдинов <nikitahysnutdinov@gmail.com>
2025-08-23 09:05:35 +0300

    Базовые объекты для описание объектов на сцене

---
+++ Rotation.cs
@@ -0,0 +1,45 @@
+﻿using SharpNEX.Engine.EngineMath;
+
+namespace SharpNEX.Engine;
+
+[Serializable]
+public struct Rotation
+{
+    public readonly float Angle;
+
+    public Rotation(float angle)
+    {
+        angle %= 360;
+        Angle = angle;
+    }
+
+    public float Radians => Trigonometry.AngleDegreesToRadians(Angle);
+
+    #region Операторы
+
+    public static Rotation operator +(Rotation a, Rotation b)
+    {
+        var result = a.Angle + b.Angle;
+        return new Rotation(result);
+    }
+
+    public static Rotation operator -(Rotation a, Rotation b)
+    {
+        var result = a.Angle - b.Angle;
+        return new Rotation(result);
+    }
+
+    public static Rotation operator +(Rotation a, float b)
+    {
+        var result = a.Angle + b;
+        return new Rotation(result);
+    }
+
+    public static Rotation operator -(Rotation a, float b)
+    {
+        var result = a.Angle - b;
+        return new Rotation(result);
+    }
+
+    #endregion
+}
---
+++ Vector.cs
@@ -0,0 +1,61 @@
+﻿namespace SharpNEX.Engine;
+
+[Serializable]
+public readonly record struct Vector(float X, float Y)
+{
+    public static readonly Vector Zero = new Vector(0, 0);
+    public static readonly Vector Left = new Vector(-1, 0);
+    public static readonly Vector Right = new Vector(1, 0);
+    public static readonly Vector Forward = new Vector(0, 1);
+    public static readonly Vector Back = new Vector(0, -1);
+
+    #region Операторы
+
+    public static Vector operator +(Vector a, Vector b)
+    {
+        return new Vector(a.X + b.X, a.Y + b.Y);
+    }
+
+    public static Vector operator -(Vector a, Vector b)
+    {
+        return new Vector(a.X - b.X, a.Y - b.Y);
+    }
+
+    public static Vector operator -(Vector a)
+    {
+        return new Vector(-a.X, -a.Y);
+    }
+
+    public static Vector operator *(Vector a, float b)
+    {
+        return new Vector(a.X * b, a.Y * b);
+    }
+
+    public static Vector operator /(Vector a, float b)
+    {
+        return new Vector(a.X / b, a.Y / b);
+    }
+
+    #endregion
+
+    public override string ToString()
+    {
+        return $"{{ X: {X}; Y: {Y} }}";
+    }
+
+    public float GetLength()
+    {
+        return (float)Math.Sqrt(X * X + Y * Y);
+    }
+
+    public Vector Normalize()
+    {
+        var length = GetLength();
+        return new Vector(X / length, Y / length);
+    }
+
+    public float Dot(Vector vector)
+    {
+        return X * vector.X + Y * vector.Y;
+    }
+}
