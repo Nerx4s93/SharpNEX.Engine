31d1c5fff30aa2ad47b806e7bc12ec6563922a00
Никита Хуснутдинов <nikitahysnutdinov@gmail.com>
2025-08-23 08:56:12 +0300

    Кеш

---
+++ Cache/CacheManager.cs
@@ -0,0 +1,41 @@
+﻿namespace SharpNEX.Engine.Cache
+{
+    internal class CacheManager<TKey, TValue>(Func<TKey, TValue> setValue)
+        where TKey : notnull
+    {
+        private readonly Dictionary<TKey, TValue> _cache = new();
+        private readonly object _lock = new();
+
+        public void Clear()
+        {
+            lock (_lock)
+            {
+                _cache.Clear();
+            }
+        }
+
+        public void Remove(TKey key)
+        {
+            lock (_lock)
+            {
+                _cache.Remove(key);
+            }
+        }
+
+        public TValue GetValue(TKey key)
+        {
+            lock (_lock)
+            {
+                if (!_cache.TryGetValue(key, out var value))
+                {
+                    value = setValue(key);
+                    if (value is null)
+                        throw new ArgumentNullException(nameof(value), "SetValue returned null");
+
+                    _cache[key] = value;
+                }
+                return value;
+            }
+        }
+    }
+}
---
+++ Cache/Data/Angle.cs
@@ -0,0 +1,4 @@
+﻿namespace SharpNEX.Engine.Cache.Data
+{
+    public readonly record struct Angle(float Radians, float Sin, float Cos);
+}
