b7186d43b77d69e20636be95eb6a19a4ba87c970
Никита Хуснутдинов <nikitahysnutdinov@gmail.com>
2025-08-24 16:14:20 +0300

    Скрипт - рендер спрайтов

--- Game.cs
+++ Game.cs
@@ -7,27 +7,27 @@ public static class Game
 {
     private static Thread? _gameThread;
 
-    private static IPlatform? _platform;
-    private static IWindow? _window;
-    private static IRenderer? _renderer;
+    public static IPlatform? Platform { get; private set; }
+    public static IWindow? Window { get; private set; }
+    public static IRenderer? Renderer { get; private set; }
 
     public static bool IsGameRun { get; private set; }
 
     public static void Run(IPlatform platform, int width, int height, List<Scene> scenes)
     {
-        _platform = platform;
+        Platform = platform;
 
-        _window = _platform.CreateWindow("Game", width, height);
-        _renderer = _platform.CreateRenderer(_window, "GDIRenderer");
+        Window = Platform.CreateWindow("Game", width, height);
+        Renderer = Platform.CreateRenderer(Window, "GDIRenderer");
 
-        _renderer.Init(_window.Hwnd, width, height);
+        Renderer.Init(Window.Hwnd, width, height);
         SceneManager.Init(scenes);
 
         _gameThread = new Thread(GameLoop);
         _gameThread.Start();
         IsGameRun = true;
 
-        _window.Show();
+        Window.Show();
     }
 
     private static void GameLoop()
@@ -38,9 +38,9 @@ public static class Game
         {
             stopwatch.Restart();
 
-            _renderer!.BeginFrame();
+            Renderer!.BeginFrame();
             SceneManager.CurrentScene!.Update();
-            _renderer.EndFrame();
+            Renderer.EndFrame();
 
             stopwatch.Stop();
             var frameTimeMs = (int)Math.Round(stopwatch.Elapsed.TotalMilliseconds);
---
+++ Scripts/SpriteRenderer.cs
@@ -0,0 +1,20 @@
+﻿using SharpNEX.Engine.Platform;
+
+namespace SharpNEX.Engine.Scripts
+{
+    public class SpriteRenderer : Script
+    {
+        public ITexture? Texture { get; set; }
+
+        public override void Update()
+        {
+            if (Texture == null)
+            {
+                return;
+            }
+
+            var position = GameObject.Position;
+            Game.Renderer!.DrawTexture(Texture, position.X, position.Y, Texture.Height, Texture.Width);
+        }
+    }
+}
